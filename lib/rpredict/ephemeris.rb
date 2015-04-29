6302d248cf9665e3a40257184e962517afc514aa
Luiz Gustavo Lourenço Moura <lglmoura@gmail.com>
2015-04-29 13:23:51 -0300

    Include Atributes Observer and Satellite in ephemeris

--- lib/rpredict/ephemeris.rb
+++ lib/rpredict/ephemeris.rb
@@ -1,13 +1,18 @@
 module RPredict
   class Ephemeris
 
-    attr_accessor :elevation, :azimuth, :range, :range_rate
+    attr_accessor :elevation, :azimuth, :range, :range_rate, :observer,
+                  :satellite, :dateTime
 
-     def initialize( azimuth = 0.0,elevation = 0.0,range = 0.0,range_rate = 0.0)
+     def initialize( observer,satellite, azimuth = 0.0,elevation = 0.0,
+                     range = 0.0,range_rate = 0.0)
+      @observer   = observer
+      @satellite  = satellite
       @elevation  = elevation
       @azimuth    = azimuth
       @range      = range
       @range_rate = range_rate
+      @dateTime   = DateTime.now
      end
   end
 end
--- spec/ephemeris_spec.rb
+++ spec/ephemeris_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe RPredict::Ephemeris  do
+  let(:name) {"TEME example"}
+  let(:line1) {"1 00005U 58002B   00179.78495062  .00000023  00000-0  28098-4 0  4753"}
+  let(:line2) {"2 00005  34.2682 348.7242 1859667 331.7664  19.3264 10.82419157413667"}
+  let(:satellite) { RPredict::Norad.select_ephemeris(RPredict::Satellite.new(name,line1,line2))}
+
+  let(:latitude) {21.40}
+  let(:longitude) {-41.50}
+  let(:altitude) {100.0}
+  let(:observer) { RPredict::Observer.new(latitude,longitude,altitude)}
+
   let(:elevation) {0.0}
   let(:azimuth) {21.7500}
   let(:range) {41.3000}
   let(:range_rate) {15.0}
-  let(:ephemeris) {RPredict::Ephemeris.new(azimuth,elevation,range,range_rate)}
+  let(:ephemeris) {RPredict::Ephemeris.new(observer,satellite, azimuth,elevation,
+                                           range,range_rate)}
+
+
+  it 'get Ephemeris Satellite name ' do
+    expect(ephemeris.satellite.tle.name).to eq name
+  end
+
+  it 'get Ephemeris Observer latitude ' do
+    expect(ephemeris.observer.geodetic.latitude).to eq latitude
+  end
 
   it 'get Ephemeris elevation ' do
     expect(ephemeris.elevation).to eq elevation
